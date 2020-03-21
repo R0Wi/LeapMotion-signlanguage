cad3fbc2f44036d2f7580c048f9672bc841d80ca
Tim Retzlaff <retzlaff-tim@gmx.de>
2020-03-21 18:27:32 +0100

    Changed character images

--- web/webroot/js/tutor.js
+++ web/webroot/js/tutor.js
@@ -1,38 +1,54 @@
-const chars = 'abcdfghijlorstvwxyz';
+const chars = 'abcdefghijklmnopqrstuvwxyz';
 const probabilityThreshold = 0.6;
-var user_score = 0;
-var goal = 'b'; // start with b
 
-function checkPrediction(){
-    $.get('/currentPrediction', '', function(data, textStatus, jqXHR) {
-        if (data.hasData) {
+// poll interval
+const interval = 500;
+
+// first character a
+var goal = 'a';
+
+function checkPrediction()
+{
+    $.get('/currentPrediction', '', function(data, textStatus, jqXHR)
+    {
+        if (data.hasData)
+        {
           var prob = getProbability(goal, data["probs"]);
-          if (prob > probabilityThreshold){
+
+          if (prob > probabilityThreshold)
+          {
             var nextIndex = Math.floor(Math.random() * chars.length);
-            if (chars[nextIndex] == goal) {
+
+            if (chars[nextIndex] == goal)
+            {
               nextIndex = (nextIndex + 1) % chars.length;
             }
 
             var nextChar = chars[nextIndex];
-
             goal = nextChar;
-            $('#realimage').attr('src', 'img/asl/' + nextChar + '.gif');
+
+            $('#realimage').attr('src', 'img/alphabet/' + nextChar + '.png');
           }
 
           setProbabilityToUi(prob);
           if (nextChar)
             $('#current_char_target_text').text(nextChar.toUpperCase());
         }
-        else{
+        else
+        {
           setProbabilityToUi(0);
         }
     });
 }
 
-function getProbability(char, probabilityTable){
-  for (let i = 0; i < probabilityTable.length; i++) {
+function getProbability(char, probabilityTable)
+{
+  for (let i = 0; i < probabilityTable.length; i++)
+  {
     const currentChar = probabilityTable[i][0];
-    if (currentChar == char){
+
+    if (currentChar == char)
+    {
       return probabilityTable[i][1];
     }
   }
@@ -40,40 +56,54 @@ function getProbability(char, probabilityTable){
   return 0;
 }
 
-function setProbabilityToUi(probability){
+function setProbabilityToUi(probability)
+{
   const rounded = Math.round(probability * 100);
+
   $('#current_char_match_text').text(rounded + " %");
 
   // Set indicator color
   const thirdThreshold = (probabilityThreshold / 3) * 100;
+
   var bgClass = null;
-  if (probability == 0){
+
+  if (probability == 0)
+  {
     bgClass = "";
   }
-  else if (rounded < thirdThreshold) {
+  else if (rounded < thirdThreshold)
+  {
     bgClass = "progress-bar-danger";
   }
-  else if (rounded < thirdThreshold * 2) {
+  else if (rounded < thirdThreshold * 2)
+  {
     bgClass = "progress-bar-warning";
   }
-  else{
+  else
+  {
     bgClass = "progress-bar-success";
   }
 
   $('#current_char_match_progress').removeClass("progress-bar-danger");
   $('#current_char_match_progress').removeClass("progress-bar-warning");
   $('#current_char_match_progress').removeClass("progress-bar-success");
-  if (bgClass){
+
+  if (bgClass)
+  {
     $('#current_char_match_progress').addClass(bgClass);
   }
-  const toHundred = (1 / probabilityThreshold) * rounded;
-  $('#current_char_match_progress').css('width', toHundred+'%').attr('aria-valuenow', toHundred);  
-  $('#current_char_match_progress').text(rounded + " %");  
 
+  const toHundred = (1 / probabilityThreshold) * rounded;
+
+  $('#current_char_match_progress').css('width', toHundred+'%').attr('aria-valuenow', toHundred);  
+  $('#current_char_match_progress').text(rounded + " %");
 }
 
-$(document).ready(function() {
-    var pollInterval = setInterval(function() {
-        checkPrediction(); // Poll server for current prediction
-    }, 200);
+$(document).ready(function()
+{
+    var pollInterval = setInterval(function()
+    {
+      // Poll server for current prediction
+      checkPrediction();
+    }, interval);
  });
